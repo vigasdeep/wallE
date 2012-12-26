d9254c353ce8c9f6841234aac9c00b217fa1be41
damanpreet26 <damanpreets26@gmail.com>
2012-12-27 01:12:47 +0530

    Update wallE.sh

--- wallE.sh
+++ wallE.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 path=$PWD
-echo $path
+#echo $path
 base="$path"
 photo=$(find $base -regex ".*jpg"  -type f | while read x; do echo "`expr $RANDOM % 1000`:$x"; done | sort -n | sed 's/[0-9]*://' | head -1)
 path="file://"$photo
