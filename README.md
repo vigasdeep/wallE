e44578eb76e9f13688ab8af3f499ea0a67fd7b35
damanpreet singh <demon@demon-pc>
2012-12-27 00:58:32 +0530

    wallE

---
+++ README.md
@@ -0,0 +1,2 @@
+1 Download the script where your wallpapers are kept.
+2 run the script there ./wallE.sh
---
+++ wallE.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+path=$PWD
+echo $path
+base="$path"
+photo=$(find $base -regex ".*jpg"  -type f | while read x; do echo "`expr $RANDOM % 1000`:$x"; done | sort -n | sed 's/[0-9]*://' | head -1)
+path="file://"$photo
+gsettings set org.gnome.desktop.background picture-uri "$path"
+
+
